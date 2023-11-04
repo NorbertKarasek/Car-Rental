528eb70125f2fdf9ad8c662bd6f8c53d9f5aebb3
Norbert Karasek <139477824+NorbertKarasek@users.noreply.github.com>
2023-11-04 01:01:48 +0100

    Added part of Cars Class
    Cars have parameters and few simple methods for now.

--- Car-Rental/Cars.cs
+++ Car-Rental/Cars.cs
@@ -1,8 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class Class1
+class Car
 {
-	public Class1()
-	{
-	}
+    private string carBrand;
+    private string carSegment;
+    private string fuel;
+    private int productionYear;
+    private int price;
+    public bool available;
+    public static List<Car> carsList = new List<Car>();
+
+    public Car(string carBrand, string carSegment, string fuel, int productionYear, int price, bool available = true)
+    {
+        this.carBrand = carBrand;
+        this.carSegment = carSegment;
+        this.fuel = fuel;
+        this.productionYear = productionYear;
+        this.price = price;
+        this.available = available;
+        carsList.Add(this);
+    }
+
+    public void getInformation()
+    {
+        Console.WriteLine($"Samochód:\n{this.carBrand}\n{this.productionYear}");
+    }
+
+    public static void getCarsList()
+    {
+        int i = 1;
+        foreach (Car car in carsList)
+        {
+            string availability = car.available ? "dostępny" : "niedostępny";
+            Console.WriteLine($"{i}|  {car.carBrand}  |  {car.carSegment}  |  {car.fuel}  |  {car.productionYear} | {availability}");
+            i++;
+        }
+    }
+
+    public static void carRent(Car oneCar)
+    {
+        if (oneCar.available)
+        {
+            oneCar.available = false;
+        }
+        else
+        {
+            Console.WriteLine("Samochód jest niedostępny");
+        }
+
+    }
 }
+
