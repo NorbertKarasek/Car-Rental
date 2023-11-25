1720d2863ff29ffce33b257f302d3ba883ac520a
Norbert Karasek <139477824+NorbertKarasek@users.noreply.github.com>
2023-11-25 14:58:34 +0100

    Update HandleCarRent.cs
    Changed error handling comments, inputs retry to correct moment of rental, and show correct price at moment of choosing a car

--- Car-Rental/HandleCarRent.cs
+++ Car-Rental/HandleCarRent.cs
@@ -24,7 +24,7 @@ namespace Handle_Car_Rental
                 }
                 else if (selectedClientId < 1 || selectedClientId > Client.ClientList.Count) // If its out of client list index
                 {
-                    Console.WriteLine("Nie ma klienta o takim numerze ID, podaj prawidłowy numer: ");
+                    Console.WriteLine("Nie ma klienta o takim numerze ID, podaj prawidłowy ID: ");
                     continue; // Ask for input again
                 }
 
@@ -34,40 +34,42 @@ namespace Handle_Car_Rental
                 Console.WriteLine($"Witamy, {selectedClient.FullName}");
                 Console.WriteLine("Który samochód chcesz wynająć? Podaj numer samochodu: ");
 
-
+                CarIdRetry:
                 if (!int.TryParse(Console.ReadLine(), out int selectedCarIndex)) // if id is not int
                 {
-                    Console.WriteLine("Błędny format ID samochodu");
-                    continue; // Ask for input again
+                    Console.Write("Błędny format ID samochodu, podaj numer: ");
+                    goto CarIdRetry; // Ask for input again
                 }
                 else if (selectedCarIndex < 1 || selectedCarIndex > Car.carsList.Count) // if its out of cars list index
                 {
-                    Console.WriteLine("Niepoprawny numer samochodu");
-                    continue; // Ask for input again
+                    Console.Write("Nie ma takiego samochodu, wybierz poprawy numer: ");
+                    goto CarIdRetry; // Ask for input again
                 }
                 else if (!Car.carsList[selectedCarIndex - 1].available) // if car is unavailable
                 {
-                    Console.WriteLine("Samochód jest niedostępny");
-                    continue;
+                    Console.Write("Samochód jest niedostępny, wybierz inny: ");
+                    goto CarIdRetry;
                 }
 
                 Car selectedCar = Car.GetCarByIndex(selectedCarIndex); // Set selected car in variable
-                if (DLicenseDuration < 4 && selectedCar.carSegment == "premium")
+
+                if (DLicenseDuration < 4 && selectedCar.carSegment == "premium") // check car segment and dlicense duration
                 {
-                    Console.WriteLine("Z uwagi na krótki okres posiadania prawa jazdy, nie możesz wynjąć auta segmentu premium");
-                    continue;
+                    Console.Write("Z uwagi na krótki okres posiadania prawa jazdy, nie możesz wynjąć auta segmentu premium\nWybierz inny samochód: ");
+                    goto CarIdRetry;
                 }
+                int priceForClient = DLicenseDuration < 4 ? (int)(selectedCar.price * 1.20) : selectedCar.price;
+                Console.WriteLine($"Cena za jeden dzień wynajmu {selectedCar.carBrand} to {priceForClient} PLN");
+                Console.WriteLine("Na ile dni chcesz wynająć auto? ");
 
-                Console.WriteLine($"Cena za jeden dzień wynajmu {selectedCar.carBrand} to {selectedCar.price} PLN");
-                Console.Write("Na ile dni chcesz wynająć auto? ");
-
+                RentDaysRetry:
                 if (!int.TryParse(Console.ReadLine(), out int rentalDuration)) // if amount of days is not int
                 {
-                    Console.WriteLine("Błędny format tekstu, podaj liczbę");
-                    continue; // Ask for input again
+                    Console.Write("Błędny format tekstu, podaj liczbę dni: ");
+                    goto RentDaysRetry; // Ask for input again
                 }
 
-                int TotalCost = DLicenseDuration < 4 ? (int)(rentalDuration * (selectedCar.price * 1.20)) : rentalDuration * selectedCar.price; // Price grows by 20% if Driver license shorter than 4 years
+                int TotalCost = priceForClient * rentalDuration;
                 var additionalDays = 0;
 
                 if (rentalDuration >= 7 && rentalDuration < 30)
@@ -81,8 +83,8 @@ namespace Handle_Car_Rental
 
                 var ReturnDate = DateTime.Now.AddDays(rentalDuration + additionalDays);
 
-                Console.WriteLine($"Całkowity koszt wynajmu to {TotalCost}");
                 Console.WriteLine($"Samochód należy oddać w dniu {ReturnDate.ToShortDateString()}");
+                Console.WriteLine($"Całkowity koszt wynajmu to {TotalCost}");
                 Car.RentCar(selectedCar);
                 break;
             }
